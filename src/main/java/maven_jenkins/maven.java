95b688d8f98b4f6f3fd023d68ef508f9d43da83c
Samiksha_khode <e40047831@rcinnet.rockwellcollins.com>
2023-04-08 23:44:35 +0530

    maven

---
+++ src/main/java/maven_jenkins/maven.java
@@ -0,0 +1,15 @@
+package maven_jenkins;
+
+public class maven {
+	public static void main(String args[]) {
+     int num=Integer.parseInt(args[0]);
+	    if(num % 2==0)
+	    {
+	    	System.out.println(num+ " is even no.");
+	    }
+	    else
+	    {
+	    	System.out.println(num+ " is odd no.");
+	    }
+}
+}
